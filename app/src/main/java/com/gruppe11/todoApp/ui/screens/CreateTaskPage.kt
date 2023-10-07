0d264010252e8536f7f51c755f001e0de53865e3
Søren Wünsche <32257201+Arcspider@users.noreply.github.com>
2023-10-07 22:08:16 +0200

    First iteration for the "Create task page"

--- app/src/main/java/com/gruppe11/todoApp/ui/screens/CreateTaskPage.kt
+++ app/src/main/java/com/gruppe11/todoApp/ui/screens/CreateTaskPage.kt
@@ -3,13 +3,29 @@ package com.gruppe11.todoApp.ui.screens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gruppe11.todoApp.ui.theme.TODOAPPTheme
 
 class CreateTaskPage : ComponentActivity() {
@@ -17,30 +33,93 @@ class CreateTaskPage : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TODOAPPTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CreateTaskContent()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateTaskContent() {
+    var taskTitle by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
+
+            Text(
+                text = "Create task",
+                style = MaterialTheme.typography.bodyLarge,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+
+        // Task Title Input
+        OutlinedTextField(
+            value = taskTitle,
+            onValueChange = { taskTitle = it },
+            label = { Text("New task") },
+            textStyle = TextStyle(fontSize = 30.sp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+
+        // Spacer to add some space between input and buttons
+        Spacer(modifier = Modifier.weight(1f))
+
+        // Buttons
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Cancel Button
+            Button(
+                onClick = {}, modifier = Modifier.weight(1f)
+            ) {
+                Text("Cancel")
+            }
+
+            // Create Button
+            Button(
+                onClick = {}, modifier = Modifier.weight(1f)
+            ) {
+                Text("Create")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CreateTaskPreview() {
     TODOAPPTheme {
-        Greeting("Android")
+        CreateTaskContent()
     }
 }
