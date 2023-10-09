a07366f39dfdaad8f6629e4d0d86cf18f1dbc350
andreasdan <andreas.petersen01@gmail.com>
2023-10-09 11:27:26 +0200

    added interface and implementation for temp database

---
+++ app/src/main/java/com/gruppe11/todoApp/model/Priority.java
@@ -0,0 +1,8 @@
+package com.gruppe11.todoApp.model;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+
+}
---
+++ app/src/main/java/com/gruppe11/todoApp/repository/TaskRepositoryImpl.kt
@@ -0,0 +1,35 @@
+package com.gruppe11.todoApp.repository
+
+import com.gruppe11.todoApp.model.Task
+
+class TaskRepositoryImpl : ITaskRepository {
+    private var id = 0
+    private val tasks: MutableList<Task?> = ArrayList()
+    override fun createTask(task: Task?): Task {
+        task!!.id = id++
+        tasks.add(task)
+        return task
+    }
+
+    override fun read(id: Int): Task? {
+        return tasks
+            .stream()
+            .filter { e: Task? -> e!!.id == id }
+            .findFirst()
+            .orElse(null)
+    }
+
+    override fun readAll(): List<Task?> {
+        return tasks
+    }
+
+    override fun update(task: Task): Task {
+        val index = tasks.indexOfFirst { e: Task? -> e!!.id == task.id }
+        tasks[index] = task
+        return task
+    }
+
+    override fun delete(task: Task) {
+        tasks.removeIf { e: Task? -> e!!.id == task.id }
+    }
+}
