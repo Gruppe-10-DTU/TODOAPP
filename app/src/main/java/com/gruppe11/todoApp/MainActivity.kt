6501ba5c97b77bce17a6be60b41c4ad6098b5089
sand4570 <sand4570@stud.kea.dk>
2023-10-12 15:03:28 +0200

    Create add task button

--- app/src/main/java/com/gruppe11/todoApp/MainActivity.kt
+++ app/src/main/java/com/gruppe11/todoApp/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.gruppe11.todoApp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addTask: View = findViewById(R.id.floatingActionButton)
+        addTask.setOnClickListener{
+            //Navigate to create task page
+        }
+    }
+
+    private fun onAddButtonClicked() {
 
     }
 }
