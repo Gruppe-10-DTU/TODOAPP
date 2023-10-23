13cc18a6b5265e7efb46a78d8ab414fb20add0a3
nts96 <s224317@student.dtu.dk>
2023-10-23 08:45:42 +0200

    ShowTaskList is now the startView.

--- app/src/main/java/com/gruppe11/todoApp/MainActivity.kt
+++ app/src/main/java/com/gruppe11/todoApp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.gruppe11.todoApp
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import com.gruppe11.todoApp.ui.screens.ShowTaskList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setContent {
+            ShowTaskList()
+        }
 
     }
 }
