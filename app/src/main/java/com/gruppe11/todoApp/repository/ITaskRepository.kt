d0c606851f609e1cea4eb44cd08a9b505dbb1974
nts96 <s224317@student.dtu.dk>
2023-10-23 08:06:20 +0200

    Task list cannot hold null values.

--- app/src/main/java/com/gruppe11/todoApp/repository/ITaskRepository.kt
+++ app/src/main/java/com/gruppe11/todoApp/repository/ITaskRepository.kt
@@ -5,7 +5,7 @@ import com.gruppe11.todoApp.model.Task
 interface ITaskRepository {
     fun createTask(task: Task): Task
     fun read(id: Int): Task?
-    fun readAll(): List<Task?>
+    fun readAll(): List<Task>
     fun update(task: Task): Task
     fun delete(task: Task)
 }
