ff08ae78158f1480712149301c69f97c1d293c1a
Philip Cramer <s224319@dtu.dk>
2024-01-02 11:27:09 +0100

    refactored to smaller composable functions ease maintainability and limit responsibilities

--- app/src/main/java/com/gruppe11/todoApp/ui/screens/CalendarScreen.kt
+++ app/src/main/java/com/gruppe11/todoApp/ui/screens/CalendarScreen.kt
@@ -1,6 +1,5 @@
 package com.gruppe11.todoApp.ui.screens
 
-import android.annotation.SuppressLint
 import android.content.res.Resources.getSystem
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -20,6 +19,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -29,7 +30,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.gruppe11.todoApp.model.Task
 import com.gruppe11.todoApp.ui.elements.DateSideScroller
+import com.gruppe11.todoApp.ui.screenStates.CalendarScreenState
 import com.gruppe11.todoApp.viewModel.CalendarViewModel
 import com.gruppe11.todoApp.viewModel.TaskViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -38,7 +41,6 @@ import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun CalendarScreen(
     viewModel: CalendarViewModel,
@@ -55,8 +57,9 @@ fun CalendarScreen(
                 viewModel = viewModel,
                 onTitleClick = {
                     CoroutineScope(Dispatchers.Main).launch {
-                        columnState.scrollTo(uiState.value.scrollState.toInt() -
-                                (getSystem().displayMetrics.densityDpi)
+                        columnState.scrollTo(
+                            uiState.value.scrollState.toInt() -
+                                    (getSystem().displayMetrics.densityDpi)
                         )
                     }
                 }
@@ -68,94 +71,160 @@ fun CalendarScreen(
                 .padding(padding)
                 .verticalScroll(state = columnState)
         ) {
-            Column(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                timeIntervals.value.forEach { time ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(timeSlotHeight.dp),
-                        contentAlignment = Alignment.TopStart
-                    ) {
-                        HorizontalDivider(
-                            Modifier
-                                .fillMaxWidth()
-                                .alpha(0.35F)
-                        )
-                        Text(
-                            text = time.format(DateTimeFormatter.ofPattern("HH:mm")),
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
+            TimeTable(
+                timeIntervals = timeIntervals,
+                slotHeight = timeSlotHeight
+            )
 
-                    }
-                }
-            }
-            Box(
-                modifier = Modifier.fillMaxWidth()
-                //.verticalScroll(state = columnState)
-            ) {
-                taskViewModel.getTaskListByDate(uiState.value.selectedDay.atStartOfDay())
-                    .filter { it.deadline.toLocalDate() == uiState.value.selectedDay && !it.isCompleted}
-                    .sortedBy { it.deadline }
-                    .forEach() { task ->
-                        val taskHeight = 60
-                        ElevatedCard(
-                            modifier = Modifier
-                                .offset(
-                                    x = 50.dp,
-                                    y = (
-                                            (task.deadline.hour.times(timeSlotHeight)
-                                                    + task.deadline.minute.times(timeSlotHeight/60))
-                                            ).dp - taskHeight.dp
-                                )
-                                .size(150.dp, taskHeight.dp)
-                                .border(
-                                    width = 1.dp,
-                                    color = MaterialTheme.colorScheme.primary,
-                                    shape = RoundedCornerShape(10.dp)
-                                )
+            TaskTable(
+                viewModel = taskViewModel,
+                state = uiState,
+                slotHeight = timeSlotHeight
+            )
 
-                        ) {
-                            Column(
-                                modifier = Modifier.padding(5.dp),
-                                verticalArrangement = Arrangement.SpaceEvenly,
-                                horizontalAlignment = Alignment.CenterHorizontally
-                            ) {
-                                Text(text = task.title)
-                            }
-
-                        }
-                    }
-            }
-            if(uiState.value.selectedDay == uiState.value.currentDay) {
-                HorizontalDivider(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .offset(
-                            x = 0.dp,
-                            y = (
-                                    (LocalDateTime.now().hour.times(timeSlotHeight)
-                                            + LocalDateTime.now().minute.times(timeSlotHeight / 60))
-                                    ).dp
-                        )
-                        .onGloballyPositioned { coordinates ->
-                            viewModel.onScrollStateChange(coordinates.positionInParent().y)
-                        },
-                    color = MaterialTheme.colorScheme.primary,
-                    thickness = 2.dp
+            if (uiState.value.selectedDay == uiState.value.currentDay) {
+                CurrentTimeLine(
+                    slotHeight = timeSlotHeight,
+                    viewModel = viewModel
                 )
-                CoroutineScope(Dispatchers.Main).launch {
-                    if (uiState.value.selectedDay == uiState.value.currentDay) {
-                        columnState.scrollTo(
-                            uiState.value.scrollState.toInt() -
-                                    (getSystem().displayMetrics.densityDpi)
-                        )
-                    }
-                }
+            }
+        }
+        LaunchedEffect(true) {
+            if (uiState.value.selectedDay == uiState.value.currentDay) {
+                columnState.scrollTo(
+                    uiState.value.scrollState.toInt() -
+                            (getSystem().displayMetrics.densityDpi)
+                )
             }
         }
     }
+}
+
+@Composable
+fun TimeTable(
+    timeIntervals : State<List<LocalDateTime>>,
+    slotHeight : Int,
+){
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        timeIntervals.value.forEach { time ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(slotHeight.dp),
+                contentAlignment = Alignment.TopStart
+            ) {
+                HorizontalDivider(
+                    Modifier
+                        .fillMaxWidth()
+                        .alpha(0.35F)
+                )
+                Text(
+                    text = time.format(DateTimeFormatter.ofPattern("HH:mm")),
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+
+            }
+        }
+    }
+
+}
+
+@Composable
+fun TaskTable(
+    viewModel: TaskViewModel,
+    state : State<CalendarScreenState>,
+    slotHeight: Int
+){
+    Box(
+        modifier = Modifier.fillMaxWidth()
+        //.verticalScroll(state = columnState)
+    ) {
+        var taskOffset = 0
+        val taskHeight = 60
+        val taskWidth = 150
+        var sideFlip = 1
+        viewModel.getTaskListByDate(state.value.selectedDay.atStartOfDay())
+            .filter { it.deadline.toLocalDate() == state.value.selectedDay && !it.isCompleted }
+            .sortedBy { it.deadline }
+            //.reversed()
+            .zipWithNext { taskA, taskB ->
+
+                CalendarTask(
+                    task = taskA,
+                    offset = 50 + taskOffset,
+                    slotHeight = slotHeight,
+                    taskHeight = taskHeight
+                )
+                if (taskA.deadline <= taskB.deadline) taskOffset += taskWidth * sideFlip
+                else taskOffset -= taskWidth * sideFlip
+                CalendarTask(task = taskB,
+                    offset = 50 + taskOffset,
+                    slotHeight = slotHeight,
+                    taskHeight = taskHeight
+                )
+                sideFlip *= -1
+            }
+    }
+}
+
+@Composable
+fun CalendarTask(
+    task : Task,
+    offset: Int,
+    slotHeight: Int,
+    taskHeight: Int
+){
+    ElevatedCard(
+        modifier = Modifier
+            .offset(
+                x = offset.dp,
+                y = (
+                        (task.deadline.hour.times(slotHeight)
+                                + task.deadline.minute.times(slotHeight / 60))
+                        ).dp - taskHeight.dp
+            )
+            .size(150.dp, taskHeight.dp)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(10.dp)
+            )
+
+    ) {
+        Column(
+            modifier = Modifier.padding(5.dp),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = task.title)
+        }
+
+    }
+}
+@Composable
+fun CurrentTimeLine(
+    slotHeight: Int,
+    viewModel: CalendarViewModel
+){
+    HorizontalDivider(
+        modifier = Modifier
+            .fillMaxWidth()
+            .offset(
+                x = 0.dp,
+                y = (
+                        (LocalDateTime.now().hour.times(slotHeight)
+                                + LocalDateTime.now().minute.times(slotHeight / 60))
+                        ).dp
+            )
+            .onGloballyPositioned { coordinates ->
+                viewModel.onScrollStateChange(coordinates.positionInParent().y)
+            },
+        color = MaterialTheme.colorScheme.primary,
+        thickness = 2.dp
+    )
+
 }
 
 @Preview
