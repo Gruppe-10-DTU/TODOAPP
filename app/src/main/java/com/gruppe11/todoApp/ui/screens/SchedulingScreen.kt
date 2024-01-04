451b12d9148c3ead8223c396d5f809d750932473
Philip Cramer <s224319@dtu.dk>
2024-01-04 21:41:50 +0100

    Switched grid to staggered horizontal and column to lazy

--- app/src/main/java/com/gruppe11/todoApp/ui/screens/SchedulingScreen.kt
+++ app/src/main/java/com/gruppe11/todoApp/ui/screens/SchedulingScreen.kt
@@ -8,36 +8,34 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.staggeredgrid.LazyHorizontalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.times
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.gruppe11.todoApp.model.Priority
@@ -45,6 +43,11 @@ import com.gruppe11.todoApp.model.Task
 import com.gruppe11.todoApp.ui.elements.DateSideScroller
 import com.gruppe11.todoApp.viewModel.CalendarViewModel
 import com.gruppe11.todoApp.viewModel.TaskViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 
 @Composable
@@ -55,36 +58,44 @@ fun SchedulingScreen(
     val timeslots: List<IntRange> =  listOf((0 .. 5), (6..11), (12..17), (18..23))// TODO Fetch list from persistent storage
     var timeSlotHeight by remember { mutableStateOf(0) } // TODO Proper calculation of slot height
     val uiState = viewModel.uiState.collectAsStateWithLifecycle()
-    val columnScrollState = rememberScrollState()
+    val columnScrollState = rememberLazyListState()
     val taskList = taskViewModel.getTaskListByDate(uiState.value.selectedDay.atStartOfDay())
-    val taskHeight = 80.dp
-    val taskWidth = 100.dp
+    val taskHeight = 60.dp
+    val taskWidth = 60.dp
 
 
     Scaffold(
         topBar = {
             DateSideScroller(
                 viewModel = viewModel,
-                onTitleClick = {  }
+                onTitleClick = {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        scrollToCurrentTime(state = columnScrollState, slots = timeslots)
+                    }
+                }
             )
         }
     ) { padding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .padding(padding)
                 .fillMaxSize()
                 .onGloballyPositioned { coordinates ->
                     timeSlotHeight = coordinates.size.height
-                }
-                //.verticalScroll(columnScrollState)
+                },
+            state = columnScrollState
         ) {
-            timeslots.forEach() {slot ->
+            items(timeslots){slot ->
+                val slotHeight = (24 / (slot.last - slot.first)).times(50).dp
                 TimeSlot(
                     timeSlot = slot,
-                    slotHeight = (timeSlotHeight / timeslots.size).dp
+                    slotHeight = slotHeight
                 ) {
-                    LazyVerticalGrid(columns = GridCells.Adaptive(taskWidth)) {
-                        items(taskList.filter { slot.contains(it.deadline.hour) }  // TODO implement timeslot data class
+                    LazyHorizontalStaggeredGrid(rows = StaggeredGridCells.Fixed(2)) {
+                        items(items = taskList // TODO implement timeslot data class
+                            .filter { slot.contains(it.deadline.hour) }
+                            .sortedBy { it.priority }
+                            .reversed()
                         ) { task ->
                             ScheduleTask(
                                 task = task,
@@ -97,22 +108,27 @@ fun SchedulingScreen(
                 HorizontalDivider()
             }
         }
+        LaunchedEffect(key1 = LocalDateTime.now().hour) {
+            if (uiState.value.selectedDay == LocalDate.now()) {
+                scrollToCurrentTime(state = columnScrollState, slots = timeslots)
+            }
+        }
     }
 }
 
 @Composable
 fun TimeSlot(
     slotHeight: Dp,
-    timeSlot: IntRange,
+    timeSlot: IntRange, // TODO Change type to match timeslot implementation
     content: @Composable () -> Unit
 ){
     HorizontalDivider()
     Row(modifier = Modifier
-        .defaultMinSize(minHeight = 150.dp)
+        .height(slotHeight)
     ) {
         Column(
             modifier = Modifier
-                .defaultMinSize(minHeight = 150.dp)
+                .defaultMinSize(minHeight = slotHeight)
                 .padding(5.dp, 10.dp),
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
@@ -138,24 +154,25 @@ fun ScheduleTask(
     height: Dp,
     width: Dp
 ){
-    val prioColor = when (task.priority) {
+    val priorityColor = when (task.priority) {
         Priority.HIGH -> Color.Red
         Priority.MEDIUM -> MaterialTheme.colorScheme.primary
         Priority.LOW -> Color.Green
     }
     ElevatedCard(
         modifier = Modifier
-            .size(width, height)
+            .defaultMinSize(minWidth = width, minHeight = height)
             .padding(2.dp)
+            .fillMaxSize()
             .border(
                 width = 1.dp,
-                color = prioColor,
+                color = priorityColor,
                 shape = RoundedCornerShape(10.dp)
             )
 
     ) {
         Column(
-            modifier = Modifier.padding(5.dp),
+            modifier = Modifier.padding(10.dp),
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -163,5 +180,15 @@ fun ScheduleTask(
             // TODO add relevant info such as priority etc.
         }
 
+    }
+}
+
+suspend fun  scrollToCurrentTime(
+    state: LazyListState,
+    slots: List<IntRange> // TODO Change type to match timeslot implementation
+){
+    slots.forEach{
+        if (it.contains(LocalDateTime.now().hour))
+        state.scrollToItem(slots.indexOf(it))
     }
 }
