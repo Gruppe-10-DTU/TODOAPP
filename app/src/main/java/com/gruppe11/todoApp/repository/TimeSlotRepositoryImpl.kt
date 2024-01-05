745be96cdc7599738a92701720d51b3fab4e569e
Philip Cramer <s224319@dtu.dk>
2024-01-05 10:43:42 +0100

    created TimeSlotRepositoryImpl

---
+++ app/src/main/java/com/gruppe11/todoApp/repository/TimeSlotRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.gruppe11.todoApp.repository
+
+import com.gruppe11.todoApp.model.TimeSlot
+
+class TimeSlotRepositoryImpl: ITimeSlotRepository {
+
+
+
+    override fun create(timeSlot: TimeSlot): TimeSlot {
+        TODO("Not yet implemented")
+    }
+
+    override fun read(): List<TimeSlot> {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(timeSlot: TimeSlot): TimeSlot? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(timeSlot: TimeSlot) {
+        TODO("Not yet implemented")
+    }
+}
