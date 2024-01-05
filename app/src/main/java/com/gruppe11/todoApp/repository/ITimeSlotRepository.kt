11a3ee334c6530d940c2d7590af6f79f3c893b23
Philip Cramer <s224319@dtu.dk>
2024-01-05 16:33:46 +0100

    renamed read to read all and changed type to Flow

--- app/src/main/java/com/gruppe11/todoApp/repository/ITimeSlotRepository.kt
+++ app/src/main/java/com/gruppe11/todoApp/repository/ITimeSlotRepository.kt
@@ -1,11 +1,12 @@
 package com.gruppe11.todoApp.repository
 
 import com.gruppe11.todoApp.model.TimeSlot
+import kotlinx.coroutines.flow.Flow
 
 interface ITimeSlotRepository {
 
     fun create(timeSlot: TimeSlot): TimeSlot
-    fun read(): List<TimeSlot>
+    fun readAll(): Flow<List<TimeSlot>>
     fun update(timeSlot: TimeSlot): TimeSlot?
     fun delete(timeSlot: TimeSlot)
 
