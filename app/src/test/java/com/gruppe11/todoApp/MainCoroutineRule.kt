31e4cdf6dcef222bf485173dcd5a323cead30eec
nts96 <s224317@student.dtu.dk>
2024-01-08 13:52:14 +0100

    Current state of unittests

---
+++ app/src/test/java/com/gruppe11/todoApp/MainCoroutineRule.kt
@@ -0,0 +1,27 @@
+package com.gruppe11.todoApp
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/*
+    Fix for coroutine test
+    https://stackoverflow.com/questions/58303961/kotlin-coroutine-unit-test-fails-with-module-with-the-main-dispatcher-had-faile
+ */
+class MainCoroutineRule constructor(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
--- app/src/test/java/com/gruppe11/todoApp/viewModel/TaskViewModelTest.kt
+++ app/src/test/java/com/gruppe11/todoApp/viewModel/TaskViewModelTest.kt
@@ -1,15 +1,101 @@
 package com.gruppe11.todoApp.viewModel
 
+import com.gruppe11.todoApp.MainCoroutineRule
+import com.gruppe11.todoApp.model.Task
+import com.gruppe11.todoApp.model.Priority
+import com.gruppe11.todoApp.repository.ITaskRepository
+import com.gruppe11.todoApp.repository.ISubtaskRepository
+import com.gruppe11.todoApp.repository.SubtaskRepositoryImpl
 import junit.framework.TestCase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import java.time.LocalDateTime
+import io.mockk.every
+import io.mockk.verify
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit4.MockKRule
+import io.mockk.verify
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-
+import java.net.UnknownHostException
 
 class TaskViewModelTest() {
 
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    val mockkRule = MockKRule(this)
+
+    @MockK(relaxed = true)
+    lateinit var mockTaskRepository: ITaskRepository
+
+    private fun getTasks() : List<Task> {
+        return listOf(
+            Task(1, "test 1", Priority.LOW, LocalDateTime.now().minusDays(1), false),
+            Task(2, "test 2", Priority.MEDIUM, LocalDateTime.now().minusDays(1), false),
+            Task(3, "test 3", Priority.HIGH, LocalDateTime.now().minusDays(1), false),
+            Task(4, "test 4", Priority.LOW, LocalDateTime.now(), false),
+            Task(5, "test 5", Priority.MEDIUM, LocalDateTime.now(), false),
+            Task(6, "test 6", Priority.HIGH, LocalDateTime.now(), false)
+        )
+    }
+
+
+    private fun getTasksFlow(
+        deferred: CompletableDeferred<Unit>? = null
+    ) : Flow<List<Task>> {
+        val tasksList = getTasks()
+        val tasksFlow : Flow<List<Task>> = flow {
+            deferred?.await()
+            emit(tasksList)
+        }
+        return tasksFlow
+    }
+
 
     @Test
-    fun testing()  {
+    fun filterTasksByPriority() = runTest {
+        val deferred = CompletableDeferred<Unit>()
 
 
+        every { mockTaskRepository.readAll() } returns getTasksFlow(deferred)
+
+
+        val viewmodel = TaskViewModel(mockTaskRepository, SubtaskRepositoryImpl())
+
+        getTasksFlow(deferred)
+
+        val defaultTasks = viewmodel.TaskState.first()
+
+        assertEquals(3, defaultTasks)
+
+        viewmodel.addPriority(Priority.HIGH)
+
+        val filteredTasks = viewmodel.TaskState.first()
+        assertEquals(1, filteredTasks.size)
+
+    }
+
+    @Test
+    fun filterTasksByDateCorrectly() = runTest {
+        val deferred = CompletableDeferred<Unit>()
+
+        mockTaskRepository.apply{
+            every{readAll()} returns getTasksFlow(deferred)
+        }
+        val viewModel = TaskViewModel(mockTaskRepository,SubtaskRepositoryImpl())
+        val taskList = viewModel.TaskState.first()
+        assertEquals(3, taskList.size)
     }
 }
