2670e2903a19ea18682551536bbe015dc535b2ff
nts96 <s224317@student.dtu.dk>
2024-01-08 14:42:31 +0100

    Added tests for tasksviewmodel

--- app/src/test/java/com/gruppe11/todoApp/viewModel/TaskViewModelTest.kt
+++ app/src/test/java/com/gruppe11/todoApp/viewModel/TaskViewModelTest.kt
@@ -17,6 +17,7 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.junit4.MockKRule
 import io.mockk.verify
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -49,7 +50,7 @@ class TaskViewModelTest() {
             Task(1, "test 1", Priority.LOW, LocalDateTime.now().minusDays(1), false),
             Task(2, "test 2", Priority.MEDIUM, LocalDateTime.now().minusDays(1), false),
             Task(3, "test 3", Priority.HIGH, LocalDateTime.now().minusDays(1), false),
-            Task(4, "test 4", Priority.LOW, LocalDateTime.now(), false),
+            Task(4, "test 4", Priority.LOW, LocalDateTime.now(), true),
             Task(5, "test 5", Priority.MEDIUM, LocalDateTime.now(), false),
             Task(6, "test 6", Priority.HIGH, LocalDateTime.now(), false)
         )
@@ -59,7 +60,6 @@ class TaskViewModelTest() {
     private fun getTasksFlow(
         deferred: CompletableDeferred<Unit>? = null
     ) : Flow<List<Task>> {
-        println("MOcking flows")
         val tasksList = getTasks()
         val tasksFlow = flow {
             emit(tasksList)
@@ -73,7 +73,6 @@ class TaskViewModelTest() {
     fun filterTasksByPriority() = runTest {
         val deferred = CompletableDeferred<Unit>()
 
-
         every { mockTaskRepository.readAll() } returns getTasksFlow(deferred)
 
         val viewmodel = TaskViewModel(mockTaskRepository, SubtaskRepositoryImpl())
@@ -86,8 +85,41 @@ class TaskViewModelTest() {
 
         val filteredTasks = viewmodel.TaskState.first()
         assertEquals(1, filteredTasks.size)
-
+        assertEquals("test 6", filteredTasks[0].title)
     }
+
+    @Test
+    fun filterTasksByComplete() = runTest {
+        val deferred = CompletableDeferred<Unit>()
+
+        every { mockTaskRepository.readAll() } returns getTasksFlow(deferred)
+
+        val viewmodel = TaskViewModel(mockTaskRepository, SubtaskRepositoryImpl())
+
+        viewmodel.changeFilter("complete")
+
+        val tasks = viewmodel.TaskState.first()
+        assertEquals(1, tasks.size)
+        assertEquals("test 4", tasks[0].title)
+    }
+
+    @Test
+    fun filterTasksByIncomplete() = runTest {
+        val deferred = CompletableDeferred<Unit>()
+
+        every { mockTaskRepository.readAll() } returns getTasksFlow(deferred)
+
+        val viewmodel = TaskViewModel(mockTaskRepository, SubtaskRepositoryImpl())
+
+        viewmodel.changeFilter("incomplete")
+
+        val tasks = viewmodel.TaskState.first()
+        assertEquals(2, tasks.size)
+        assertEquals("test 6", tasks[0].title)
+        assertEquals("test 5", tasks[1].title)
+    }
+
+
 
     @Test
     fun filterTasksByDateCorrectly() = runTest {
@@ -96,8 +128,60 @@ class TaskViewModelTest() {
         mockTaskRepository.apply{
             every{readAll()} returns getTasksFlow(deferred)
         }
+
         val viewModel = TaskViewModel(mockTaskRepository,SubtaskRepositoryImpl())
-        val taskList = viewModel.TaskState.first()
+
+        var taskList = viewModel.TaskState.first()
         assertEquals(3, taskList.size)
+        assertEquals("test 6", taskList[0].title)
+
+        viewModel.changeDate(LocalDateTime.now().minusDays(1))
+        taskList = viewModel.TaskState.first()
+        assertEquals(3,taskList.size)
+        assertEquals("test 3", taskList[0].title)
+
+        viewModel.changeDate(LocalDateTime.now().minusDays(2))
+        taskList = viewModel.TaskState.first()
+        assertTrue(taskList.isEmpty())
+    }
+
+    @Test
+    fun sortTaskByPriority() = runTest{
+        val deferred = CompletableDeferred<Unit>()
+
+        mockTaskRepository.apply{
+            every{readAll()} returns getTasksFlow(deferred)
+        }
+        val viewModel = TaskViewModel(mockTaskRepository,SubtaskRepositoryImpl())
+        var taskList = viewModel.TaskState.first()
+        assertEquals(Priority.HIGH,taskList.first().priority)
+        viewModel.selectSortingOption("Priority Ascending")
+        taskList = viewModel.TaskState.first()
+
+        assertEquals(Priority.LOW,taskList.first().priority)
+
+        viewModel.selectSortingOption("Priority Descending")
+        taskList = viewModel.TaskState.first()
+        assertEquals(Priority.HIGH,taskList.first().priority)
+
+    }
+
+    @Test
+    fun sortTasksByTitle() = runTest {
+        val deferred = CompletableDeferred<Unit>()
+
+        mockTaskRepository.apply{
+            every{readAll()} returns getTasksFlow(deferred)
+        }
+        val viewModel = TaskViewModel(mockTaskRepository,SubtaskRepositoryImpl())
+        viewModel.selectSortingOption("A-Z")
+
+        var taskList = viewModel.TaskState.first()
+        assertEquals("test 4",taskList.first().title)
+
+        viewModel.selectSortingOption("Z-A")
+        taskList = viewModel.TaskState.first()
+
+        assertEquals("test 6" ,taskList.first().title)
     }
 }
