0695b895c4dc7351abb1242ee21c801154be493c
Philip Cramer <s224319@dtu.dk>
2024-01-09 14:23:12 +0100

    updated test to reflect task implementation

--- app/src/androidTest/java/com/gruppe11/todoapp/test/FilterTasks.kt
+++ app/src/androidTest/java/com/gruppe11/todoapp/test/FilterTasks.kt
@@ -24,34 +24,34 @@ class FilterTasks {
     fun iHaveCreatedMultipleTasksAndIWantToQuicklyBeAbleToGroupThemByDay() = runTest {
         for (i in 1.. 5){
             viewModel.addTask(
-                Task(i,"test", Priority.MEDIUM,LocalDateTime.now(),false),
+                Task(i,"test", Priority.MEDIUM,LocalDateTime.now(),false, emptyList()),
                 listOf()
             )
         }
-        val latedate : LocalDateTime = LocalDateTime.now().minusDays(5);
-        viewModel.addTask(Task(6,"test", Priority.MEDIUM,latedate,false)
+        val latedate : LocalDateTime = LocalDateTime.now().minusDays(5)
+        viewModel.addTask(Task(6,"test", Priority.MEDIUM,latedate,false, emptyList())
         ,
         emptyList())
     }
 
     @When("I select a date in the date selector.")
     fun iSelectADateInTheDateSelector() = runTest {
-        selectedDate = LocalDateTime.now();
+        selectedDate = LocalDateTime.now()
     }
 
     @Then("A curated selection of tasks with the corresponding selected date should appear")
     fun aCuratedSelectionOfTasksWithTheCorrespondingSelectedDateShouldAppear() = runTest {
-        val tasks : List<Task> = viewModel.getTaskListByDate(selectedDate);
-        assertEquals(5, tasks.size);
+        val tasks : List<Task> = viewModel.getTaskListByDate(selectedDate)
+        assertEquals(5, tasks.size)
         for (task : Task in tasks){
-            assertEquals(task.deadline.toLocalDate(), selectedDate.toLocalDate());
+            assertEquals(task.deadline.toLocalDate(), selectedDate.toLocalDate())
         }
     }
 
     @And("All the tasks that do not have the associated date should not be displayed")
     fun allTheTasksThatDoNotHaveTheAssociatedDateShouldNotBeDisplayed() = runTest {
         val tasks : List<Task> = viewModel.getTaskListByDate(selectedDate)
-        assertFalse(tasks.size == 6);
+        assertFalse(tasks.size == 6)
 
     }
 }
