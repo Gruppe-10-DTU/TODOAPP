655d30208de7f83a40f7a2f19379c16206dde4a0
Philip Cramer <s224319@dtu.dk>
2024-01-09 13:59:49 +0100

    updated tests to match repository implementation

--- app/src/test/java/com/gruppe11/todoApp/repository/SubtaskRepositoryImplTest.kt
+++ app/src/test/java/com/gruppe11/todoApp/repository/SubtaskRepositoryImplTest.kt
@@ -3,6 +3,9 @@ package com.gruppe11.todoApp.repository
 import com.gruppe11.todoApp.model.Priority
 import com.gruppe11.todoApp.model.SubTask
 import com.gruppe11.todoApp.model.Task
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.junit.Assert.*
 import org.junit.Before
 
@@ -15,80 +18,93 @@ class SubtaskRepositoryImplTest {
     private lateinit var task: Task
     @Before
     fun createRepo() {
-        subtaskRepository = SubtaskRepositoryImpl();
-        task =  Task(1, "test", Priority.HIGH, LocalDateTime.now(), false);
+        subtaskRepository = SubtaskRepositoryImpl()
+        task =  Task(1, "test", Priority.HIGH, LocalDateTime.now(), false, emptyList())
     }
 
     @Test
     fun createSubtaskEmpty() {
-        //Arrange
-        val subtask: SubTask = SubTask("test", 1);
+        CoroutineScope(Dispatchers.Main).launch {
 
-        //Act
-        subtaskRepository.createSubtask(task, subtask);
+            //Arrange
+            val subtask = SubTask("test", 1)
 
-        //Assert
-        val subtasks = subtaskRepository.readAll(task)
-        assertEquals(1, subtasks.size )
-        assertEquals(subtask, subtasks[0]);
+            //Act
+            subtaskRepository.createSubtask(task, subtask)
 
+            //Assert
+            val subtasks = subtaskRepository.readAll(task)
+            assertEquals(1, subtasks.size)
+            assertEquals(subtask, subtasks[0])
+
+        }
     }
     @Test
     fun createSubtaskSecond() {
-        //Arrange
-        subtaskRepository.createSubtask(task, SubTask("test", 1));
+        CoroutineScope(Dispatchers.Main).launch {
 
-        //Act
-        subtaskRepository.createSubtask(task, SubTask("test", 2));
+            //Arrange
+            subtaskRepository.createSubtask(task, SubTask("test", 1))
 
-        //Assert
-        val subtasks = subtaskRepository.readAll(task);
-        assertEquals(2, subtasks.size);
+            //Act
+            subtaskRepository.createSubtask(task, SubTask("test", 2))
+
+            //Assert
+            val subtasks = subtaskRepository.readAll(task)
+            assertEquals(2, subtasks.size)
+        }
     }
     @Test
     fun readAll() {
-        //Arrange
-        for (i in 1 .. 5) {
-            subtaskRepository.createSubtask(task, SubTask("test", i));
-        }
+        CoroutineScope(Dispatchers.Main).launch {
+            //Arrange
+            for (i in 1..5) {
+                subtaskRepository.createSubtask(task, SubTask("test", i))
+            }
 
-        //Act
-        val subtasks = subtaskRepository.readAll(task);
+            //Act
+            val subtasks = subtaskRepository.readAll(task)
 
-        //Assert
-        assertEquals(5, subtasks.size);
-        for (i in 1 .. 5){
-            assertEquals(i, subtasks[i-1].id)
+            //Assert
+            assertEquals(5, subtasks.size)
+            for (i in 1..5) {
+                assertEquals(i, subtasks[i - 1].id)
+            }
         }
     }
 
 
     @Test
     fun update() {
-        //Arrange
-        val subtask = subtaskRepository.createSubtask(task, SubTask("EditTask", 0));
-        val edited = subtask.copy(title = "NewItem")
+        CoroutineScope(Dispatchers.Main).launch {
 
-        //Act
-        subtaskRepository.update(task, edited);
-        val updatedSubtask = subtaskRepository.readAll(task)[0];
+            //Arrange
+            val subtask = subtaskRepository.createSubtask(task, SubTask("EditTask", 0))
+            val edited = subtask.copy(title = "NewItem")
 
-        //Assert
-        assertNotEquals(updatedSubtask.title, subtask.title)
-        assertEquals(edited, updatedSubtask);
+            //Act
+            subtaskRepository.update(task, edited)
+            val updatedSubtask = subtaskRepository.readAll(task)[0]
+
+            //Assert
+            assertNotEquals(updatedSubtask.title, subtask.title)
+            assertEquals(edited, updatedSubtask)
+        }
     }
 
     @Test
     fun delete() {
-        //Arrange
-        val subtask = subtaskRepository.createSubtask(task, SubTask("EditTask", 0));
-        assertEquals(1, subtaskRepository.readAll(task).size)
+        CoroutineScope(Dispatchers.Main).launch {
 
-        //Act
-        subtaskRepository.delete(task, subtask)
+            //Arrange
+            val subtask = subtaskRepository.createSubtask(task, SubTask("EditTask", 0))
+            assertEquals(1, subtaskRepository.readAll(task).size)
 
-        //Assert
-        assertEquals(0, subtaskRepository.readAll(task).size)
+            //Act
+            subtaskRepository.delete(task, subtask)
 
+            //Assert
+            assertEquals(0, subtaskRepository.readAll(task).size)
+        }
     }
 }
--- app/src/test/java/com/gruppe11/todoApp/repository/TaskRepositoryImplTest.kt
+++ app/src/test/java/com/gruppe11/todoApp/repository/TaskRepositoryImplTest.kt
@@ -1,15 +1,14 @@
 package com.gruppe11.todoApp.repository
 
-import androidx.compose.runtime.collectAsState
 import com.gruppe11.todoApp.model.Priority
 import com.gruppe11.todoApp.model.Task
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNull
 import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.flow.toCollection
-import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertNotEquals
 import org.junit.Before
@@ -20,63 +19,72 @@ class TaskRepositoryImplTest {
     private lateinit var taskRepository: TaskRepositoryImpl
     @Before
     fun createRepo() {
-        taskRepository = TaskRepositoryImpl();
+        taskRepository = TaskRepositoryImpl()
     }
 
     @Test
     fun createTask() = runTest {
         //Arrange
-        val task : Task = Task(5, "Test", Priority.HIGH,  LocalDateTime.now(),false)
+        val task = Task(5, "Test", Priority.HIGH,  LocalDateTime.now(),false, emptyList())
 
         //Act
-        val taskCreated : Task = taskRepository.createTask(task);
+        val taskCreated : Task = taskRepository.createTask(task)
 
         //Assert
-        assertNotEquals(task.id, taskCreated.id);
-        assertEquals(1, taskCreated.id);
+        assertNotEquals(task.id, taskCreated.id)
+        assertEquals(1, taskCreated.id)
         assertEquals(1, taskRepository.readAll().first().size)
     }
 
     @Test
     fun findTask() {
-        //Arrange
-        val task : Task = Task(1, "Test", Priority.HIGH,  LocalDateTime.now(),false)
-        taskRepository.createTask(task);
+        CoroutineScope(Dispatchers.Main).launch {
+            //Arrange
+            val task = Task(1, "Test", Priority.HIGH, LocalDateTime.now(), false,
+                emptyList()
+            )
+            taskRepository.createTask(task)
 
-        //Act
-        val taskFound : Task? = taskRepository.read(1);
+            //Act
+            val taskFound: Task? = taskRepository.read(1)
 
-        //Assert
-        assertEquals(task.id, taskFound?.id);
-
+            //Assert
+            assertEquals(task.id, taskFound?.id)
+        }
     }
 
     @Test
     fun findTaskNotFound() {
-        //Arrange
-        val task : Task = Task(1, "Test", Priority.HIGH,  LocalDateTime.now(),false)
-        taskRepository.createTask(task);
+        CoroutineScope(Dispatchers.Main).launch {
+            //Arrange
+            val task = Task(1, "Test", Priority.HIGH, LocalDateTime.now(), false,
+                emptyList()
+            )
+            taskRepository.createTask(task)
 
-        //Act
-        val taskFound : Task? = taskRepository.read(4);
+            //Act
+            val taskFound: Task? = taskRepository.read(4)
 
-        //Assert
-        assertNull(taskFound);
+            //Assert
+            assertNull(taskFound)
+        }
     }
 
     @Test
     fun findAllTasks() = runTest {
         //Arrange
         for (i in 1..5) {
-            val task : Task = Task(i, "Test", Priority.HIGH,  LocalDateTime.now(),false)
-            taskRepository.createTask(task);
+            val task = Task(i, "Test", Priority.HIGH,  LocalDateTime.now(),false,
+                emptyList()
+            )
+            taskRepository.createTask(task)
         }
 
         //Act
         val tasks : List<Task>  = taskRepository.readAll().first()
 
         //Assert
-        assertEquals(5, tasks.size);
+        assertEquals(5, tasks.size)
     }
 
     @Test
@@ -84,37 +92,50 @@ class TaskRepositoryImplTest {
         //Arrange
 
         //Act
-        val tasks : List<Task>  = taskRepository.readAll().first();
+        val tasks : List<Task>  = taskRepository.readAll().first()
 
         //Assert
-        assertTrue(tasks.isEmpty());
+        assertTrue(tasks.isEmpty())
     }
 
     @Test
     fun deleteTask() {
-        var task : Task = Task(1, "Test", Priority.HIGH,  LocalDateTime.now(),false)
-        task = taskRepository.createTask(task);
-        var taskFound : Task? = taskRepository.read(1);
-        assertEquals(task.id, taskFound?.id);
+        CoroutineScope(Dispatchers.Main).launch {
+            var task = Task(1, "Test", Priority.HIGH, LocalDateTime.now(), false, emptyList())
+            task = taskRepository.createTask(task)
+            var taskFound: Task? = taskRepository.read(1)
+            assertEquals(task.id, taskFound?.id)
 
-        //Act
-        taskRepository.delete(task)
+            //Act
+            taskRepository.delete(task)
 
-        //Assert
-        taskFound = taskRepository.read(1);
-        assertNull(taskFound);
+            //Assert
+            taskFound = taskRepository.read(1)
+            assertNull(taskFound)
+        }
     }
 
     @Test
     fun updateTask() {
-        var createdTask : Task = taskRepository.createTask(Task(1, "Test", Priority.HIGH,  LocalDateTime.now(),false));
+        CoroutineScope(Dispatchers.Main).launch {
+            val createdTask: Task = taskRepository.createTask(
+                Task(
+                    1,
+                    "Test",
+                    Priority.HIGH,
+                    LocalDateTime.now(),
+                    false,
+                    emptyList()
+                )
+            )
 
-        //Act
-        val updateTask = taskRepository.update(createdTask.copy(priority = Priority.LOW));
+            //Act
+            val updateTask = taskRepository.update(createdTask.copy(priority = Priority.LOW))
 
-        //Assert
-        assertEquals(updateTask.priority, Priority.LOW);
-        assertEquals(createdTask.id, updateTask.id);
-        assertNotEquals(createdTask.priority, updateTask.priority);
+            //Assert
+            assertEquals(updateTask.priority, Priority.LOW)
+            assertEquals(createdTask.id, updateTask.id)
+            assertNotEquals(createdTask.priority, updateTask.priority)
+        }
     }
 }
